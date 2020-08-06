85c801f86e2ae63316d00e93bbf2efe5d7bf4242
Hayliel <rodrigo.oi@hotmail.com>
2020-08-06 17:22:53 -0300

    ~

--- test.sh
+++ test.sh
@@ -10,9 +10,24 @@ fi
 > $file
 echo "Writing some stuff to test it out" >> $file
 echo "There's numbers as well" >> $file
+echo "123456789" >> $file
 echo "And some other random stuff" >> $file
+
+echo "Add your own stuff to the file (Esc -> Enter to end it)"
+loop=0 
+while ((loop == 0)); do
+echo -n "- "
+read something
+if [[ $something == $'\e' ]]; then
+break
+else
+echo $something >> $file
+fi
+done
 
 echo "File stuff ###"
 while read -r line; do
 echo $line
 done < $file
+
+
